ebd92e62eda7d4c0adbcaa1806d094ea3b6c5184
Julian Jabs <julian.jabs@web.de>
2016-01-18 15:14:18 +0100

    Initial commit

---
+++ info.rkt
@@ -0,0 +1,20 @@
+#lang setup/infotab
+
+(define collection 'multi)
+
+(define deps '("base"
+               "compatibility-lib"
+               "drracket"
+               "drracket-plugin-lib"
+               "gui-lib"
+               "handin"
+               "htdp-lib"
+               "net-lib"
+               "pconvert-lib"
+               "rackunit-lib"
+               "sandbox-lib"
+               "srfi-lib"
+               "web-server-lib"))
+(define build-deps '("gui-doc"
+                     "racket-doc"
+                     "scribble-lib"))
